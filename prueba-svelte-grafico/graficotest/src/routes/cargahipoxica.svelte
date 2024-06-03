94087ed475ce9c0a591795dfbcb08c0ffb97ea29
benipasto <47392632@est.ort.edu.ar>
2024-06-03 00:15:15 -0300

    feat(cargahipoxica.svelte)

---
+++ prueba-svelte-grafico/graficotest/src/routes/cargahipoxica.svelte
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HypoxIA</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <div class="logo"></div>
+                <a href="#">Inicio</a>
+                <a href="#">Perfil</a>
+            </nav>
+        </div>
+    </header>
+
+    <section id="analisis">
+        <h1>Carga hipóxica:</h1>
+        <div class="valores-carga"></div>
+        <h1>Otros valores</h1>
+        <div class="valores-otros"></div>
+    </section>
+</body>
+</html>
