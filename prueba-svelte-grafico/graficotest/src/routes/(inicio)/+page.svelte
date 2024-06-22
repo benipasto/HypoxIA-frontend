0f6f1fda68a7960695964bcfcb55f4ac749c35b8
benipasto <47392632@est.ort.edu.ar>
2024-06-21 23:41:33 -0300

    feat(inicio): html hecho

---
+++ prueba-svelte-grafico/graficotest/src/routes/(inicio)/+page.svelte
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HypoxIA</title>
+    <link rel="icon" href="logohypoxia.png">
+</head>
+<body>
+    <section id="hero">
+        <h1>Cálculo de carga hipóxica, <br>al alcance de tu mano</h1>
+        <a href="/login">
+            <button>Inicia sesión o Registrarse</button>
+        </a>
+    </section>
+
+    <section id="quienesosmos">
+        <h2>¿Qué es HypoxIA?</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo rerum, necessitatibus repudiandae ex doloribus ut consequuntur deleniti cupiditate facilis! Asperiores corporis perferendis autem minus ipsa, vero dolorem. Voluptate, molestias praesentium.</p>
+        <h2>Problemática</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos cumque, doloribus sapiente blanditiis illo dolorum deleniti, quas sequi voluptas laboriosam voluptate ipsum obcaecati, impedit earum soluta fugit sit accusantium nihil!</p>
+    </section>
+
+    <section id="consecuencias">
+        <div class="container">
+            <h2>Consecuencias de las apneas del sueño</h2>
+            <div class="carta">
+                <h3>consecuencia 1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui numquam laborum repellendus incidunt, iste aut quo praesentium cumque, voluptatum voluptate perspiciatis eligendi mollitia consequatur aliquid at ea facere consequuntur.</p>
+            </div>
+            <div class="carta">
+                <h3>consecuencia 2</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui numquam laborum repellendus incidunt, iste aut quo praesentium cumque, voluptatum voluptate perspiciatis eligendi mollitia consequatur aliquid at ea facere consequuntur.</p>
+            </div>
+            <div class="carta">
+                <h3>consecuencia 3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti qui numquam laborum repellendus incidunt, iste aut quo praesentium cumque, voluptatum voluptate perspiciatis eligendi mollitia consequatur aliquid at ea facere consequuntur.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="solucion">
+        <h1>Solución</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, sit asperiores? Magni hic itaque iusto ab illum, odio, sapiente vero velit incidunt laudantium, cupiditate blanditiis labore amet deserunt ad nesciunt?</p>
+    </section>
+
+    <section id="sobrenosotros">
+        <div class="container">
+            <h3>Sobre nosotros</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nemo nostrum reiciendis nam, ipsam beatae facere minima. Earum, odio ducimus velit corrupti laboriosam veniam sint, quod sit quo labore autem?</p>
+        </div>
+    </section>
+
+    <section id="contacto">
+        <h3>Contacto</h3>
+        <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa non sed ullam explicabo sit qui quibusdam veniam harum voluptatibus quae, accusamus velit enim doloribus consequatur, numquam quas nesciunt. Perferendis, consequuntur!</h5>
+    </section>
+
+</body>
+</html>
+
+<style>
+
+</style>
