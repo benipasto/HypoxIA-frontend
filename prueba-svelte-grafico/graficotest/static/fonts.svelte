beeeb37b9b1b2fd4b6fa1498ceeb90acae9956c7
benipasto <47392632@est.ort.edu.ar>
2024-06-21 23:39:52 -0300

    feat(fonts.svelte)
    esto me deja usar la font de lato en todos lados sin complicarmela

---
+++ prueba-svelte-grafico/graficotest/static/fonts.svelte
@@ -0,0 +1,12 @@
+<style>
+        @font-face { /*esta es la font lato (creo q no hace falta aclarar pero x si acaso xd)*/
+        font-family: "Lato";
+        src: url('698242188-Lato-Bla.eot');
+        src: url('698242188-Lato-Bla.eot?#iefix') format('embedded-opentype'),
+        url('698242188-Lato-Bla.svg#Lato Black') format('svg'),
+        url('698242188-Lato-Bla.woff') format('woff'),
+        url('698242188-Lato-Bla.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+}
+</style>
