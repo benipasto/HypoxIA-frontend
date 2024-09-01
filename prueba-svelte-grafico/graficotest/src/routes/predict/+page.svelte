e3b06b636d8b04beea45f42a551dea0c7ceae1fc
benipasto <47392632@est.ort.edu.ar>
2024-09-01 20:17:39 -0300

    feat(pruebaconexion)

---
+++ prueba-svelte-grafico/graficotest/src/routes/predict/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  const sendData = async () => {
+    const data = {
+        SpO2: 95.6,
+        Tiempo_Relativo: 123.45
+    };
+
+    const response = await fetch("'http://127.0.0.1:8000/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+    console.log("Prediction:", result.prediction);
+};
+
+sendData();
+</script>
