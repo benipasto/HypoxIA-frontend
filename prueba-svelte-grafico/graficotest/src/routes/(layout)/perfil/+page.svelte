92261e20cfbc45cbe67e7eda3c2d20c2bae88e35
benipasto <110981091+benipasto@users.noreply.github.com>
2024-11-27 23:34:54 -0300

    feat(perfil): boton de logout

--- prueba-svelte-grafico/graficotest/src/routes/(layout)/perfil/+page.svelte
+++ prueba-svelte-grafico/graficotest/src/routes/(layout)/perfil/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
     // Recuperar el mail del usuario logueado desde localStorage
     let userMail: string = JSON.parse(localStorage.getItem('user') ?? '{}').mail || '';
+
+    function logout(event: MouseEvent) {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+        localStorage.removeItem('user');
+        // Redirigir al usuario si es necesario
+        window.location.href = "/login"; // Redirigir al login después del logout
+    }
 </script>
 
 <style>
@@ -9,6 +16,16 @@
         margin: 0 auto;
         max-width: 800px;
         padding: 20px;
+    }
+
+    strong{
+        font-weight: 100;
+        font-size: 20px;
+    }
+
+    .mail{
+        border-bottom: 1px solid #000000;
+        padding: 5px 0px;
     }
 
     .link {
@@ -23,20 +40,22 @@
     }
 
     .menu {
-        margin-top: 20px;
+        margin-top: 50px;
     }
 
     .menu-title {
-        font-size: 24px;
+        font-size: 32px;
         text-align: center;
         border-bottom: 2px solid #27A69E;
         margin-bottom: 20px;
+        font-weight: 100;
+        padding: 10px 0px;
     }
 
     .menu-items {
         max-height: 300px; /* Altura máxima del scroll */
         overflow-y: auto;
-        border: 1px solid #ccc;
+        border: 0px solid #ccc;
         border-radius: 8px;
         padding: 10px;
     }
@@ -45,11 +64,16 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 10px;
+        padding: 15px;
         margin-bottom: 10px;
         background-color: #CACACA;
         border-radius: 12px;
         border: 2px solid #27A69E;
+    }
+
+    span{
+        font-size: 18px;
+        margin-right: 10px;
     }
 
     .menu-item:hover {
@@ -64,9 +88,9 @@
 <div class="container">
 
     <div class="user-info">
-        <p><strong>Mail:</strong></p>
-        <p>{userMail}</p>
-        <a href="/login" class="link">Si desea cambiar de usuario, haga click aquí</a>
+        <p><strong>Mail</strong></p>
+        <p class="mail">{userMail}</p>
+        <a href="#" on:click={logout} class="link">Si desea cambiar de usuario, haga click aquí</a>
     </div>
 
     <div class="menu">
